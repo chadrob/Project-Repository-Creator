ca053129fd2863042c099d5b240d480d2994d094
Chad James <robertsonchad1@gmail.com>
2020-11-02 19:47:29 -0600

    Fixed screenshot path

--- projectCreator.js
+++ projectCreator.js
@@ -15,13 +15,11 @@ const newFile = process.argv[2];
         page.evaluate(() => {
             document.querySelector("body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.position-fixed.top-0.right-0.bottom-0.height-fit.position-lg-relative.d-lg-flex.flex-justify-between.flex-items-center.flex-auto > div.d-lg-flex.flex-items-center.px-3.px-lg-0.text-center.text-lg-left > a.HeaderMenu-link.no-underline.mr-3").click();
         })
-      ]);
-   
+      ]);   
     // take screenshot
-    await page.screenshot({path: 'screenshot.png'});
+    await page.screenshot({path: '/Users/Chad/Documents/myProjects/projectCreator/screenshot.png'});
 
     // close browser
     await browser.close();
-    return 0;
 })();
 
